1a351f497113b55e79e3a67a82872b7efb3636e9
manraj <mkooner@live.com>
2024-04-24 17:29:09 +0100

    Add basic operations

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,33 @@
-const add = function(arg, arg1) {
-    return arg + arg1;
-  };
-  
-  const subtract = function(arg, arg1) {
-    return arg - arg1;
-  };
-  
-  const sum = function(...args) {
-      return args.flat().reduce((total, arg) => total + arg, 0);
-  };
-  
-  const multiply = function(...args) {
-    return args.flat().reduce((total,arg) => total * arg, 1);
-  };
-  
-  const power = function(arg1, arg2) {
-      return arg1**arg2;
-  };
-  
-  const factorial = function(arg) {
-    return arg === 0 ? 1 : arg*factorial(arg-1);
-      
-  };
+function add(a,b) {
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+const TOKEN_DIV = '/';
+const TOKEN_MUL = '*';
+const TOKEN_ADD = '+';
+const TOKEN_SUB = '-';
+
+function operate(a,b,operator) {
+    switch(operator){
+        case TOKEN_DIV: return divide(a,b);
+        case TOKEN_MUL: return multiply(a,b);
+        case TOKEN_SUB: return subtract(a,b); 
+        case TOKEN_ADD: return add(a,b);
+    }
+}
+
+//----------------------------------------------------
+let operand1, operand2;
+let operator;

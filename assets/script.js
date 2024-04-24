275693313608eb26f756fa19ea15bb70cdb1681d
manraj <mkooner@live.com>
2024-04-24 17:13:38 +0100

    Create initial template

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+const add = function(arg, arg1) {
+    return arg + arg1;
+  };
+  
+  const subtract = function(arg, arg1) {
+    return arg - arg1;
+  };
+  
+  const sum = function(...args) {
+      return args.flat().reduce((total, arg) => total + arg, 0);
+  };
+  
+  const multiply = function(...args) {
+    return args.flat().reduce((total,arg) => total * arg, 1);
+  };
+  
+  const power = function(arg1, arg2) {
+      return arg1**arg2;
+  };
+  
+  const factorial = function(arg) {
+    return arg === 0 ? 1 : arg*factorial(arg-1);
+      
+  };
